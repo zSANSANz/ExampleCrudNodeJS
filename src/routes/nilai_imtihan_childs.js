b33971713dde7722ba6d33654e674785a2bde255
SandiIceMessanger <sandi@indochat.co.id>
2022-08-01 15:14:30 +0700

    fix: adding nilai_imtihan_childs list and create (router)

--- src/routes/nilai_imtihan_childs.js
+++ src/routes/nilai_imtihan_childs.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const nilaiImtihanChildController = require('../controllers/nilai_imtihan_childs');
 router.get('/', nilaiImtihanChildController.getAll);
-// router.post('/', nilaiImtihanChildController.create);
+router.post('/', nilaiImtihanChildController.create);
 // router.get('/:nilaiImtihanChildId', nilaiImtihanChildController.getById);
 // router.put('/:nilaiImtihanChildId', nilaiImtihanChildController.updateById);
 // router.delete('/:nilaiImtihanChildId', nilaiImtihanChildController.deleteById);

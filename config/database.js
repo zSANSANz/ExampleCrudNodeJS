e65c247e615a48bb685a066c79026d53403c9787
SandiIceMessanger <sandi@indochat.co.id>
2022-08-04 10:29:05 +0700

    first commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://sandi:1TDXtyJTT4AnOY14@cluster0.cpjef.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true });
-// mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
 
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
